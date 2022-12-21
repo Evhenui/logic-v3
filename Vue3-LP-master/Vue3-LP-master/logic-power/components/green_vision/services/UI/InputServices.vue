d18ade9df1f8dc615cc1e68164302b2bb461882a
Yevhen Vitchenko <73481852+Evhenui@users.noreply.github.com>
2022-12-21 12:54:42 +0200

    add input phone

--- Vue3-LP-master/Vue3-LP-master/logic-power/components/green_vision/services/UI/InputServices.vue
+++ Vue3-LP-master/Vue3-LP-master/logic-power/components/green_vision/services/UI/InputServices.vue
@@ -12,7 +12,21 @@
       v-model="inputValue"
     />
     <div class="input__number-country">
-      
+      <div class="input__selected">
+        <div class="input__flag">
+          <svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <rect width="20" height="16" rx="2" fill="#F6A500"/>
+            <path d="M0 2C0 0.895431 0.895431 0 2 0H18C19.1046 0 20 0.895431 20 2V8H0V2Z" fill="#4490BB"/>
+          </svg>
+        </div>
+        <span class="input__phone">+380</span>
+        <div class="input__arrow">
+          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M4 6L8 10L12 6" stroke="#0E0F0F" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+          </svg>
+        </div>
+      </div>
+      <div class="input__list"></div>
     </div>
     <label class="input__label">{{ placeholder }}</label>
   </div>
@@ -32,6 +46,27 @@ defineProps({
 <style lang="scss" scoped>
 .input {
   position: relative;
+
+  &.phone {
+
+    .input__data {
+      padding: 16px 16px 16px 117px;
+    }
+
+    .input__number-country {
+      display: block;
+
+      position: absolute;
+      top: 19px;
+      left: 16px;
+
+      cursor: pointer;
+    }
+
+    .input__label {
+      left: 117px;
+    }
+  }
 
   &__data {
     max-width: 363px;
@@ -61,6 +96,29 @@ defineProps({
       border-color: #4724fb;
       color: rgba(0, 0, 0, 0.87);
     }
+  }
+
+  &__number-country {
+    display: none;
+  }
+
+  &__selected {
+    @include flex-container(row, flex-start, center);
+
+    gap: 4px;
+  }
+
+  &__flag {
+    font-size: 0;
+  }
+
+  &__phone {
+    @include font(16, 16, 400);
+    color: #0E0F0F;
+  }
+
+  &__arrow {
+    font-size: 0;
   }
 
   &__data:focus {
